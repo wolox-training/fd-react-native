f6b9227a3de1f407304f268c3e6e27173f321f93
Fede <federicodiaz@wolox.com.ar>
2018-10-04 09:31:59 -0300

    adding both exercises, should be working

--- react/ecmascript/01 - spread/index.js
+++ react/ecmascript/01 - spread/index.js
@@ -1,10 +1,33 @@
 import { isArray } from './utils';
 
-export function min() {
-  isArray();
-  return Math.min();
+export function min(arg) {
+  if (arguments.length === 1) {
+    if (isArray(arg)) {
+      return Math.min(...arg);
+    }
+    return Math.min(arg);
+  } else if (arguments.length > 1) {
+    const args = [...arguments];
+    return Math.min(...args);
+  }
+  return undefined;
 }
 
-export function copy() {
+export function copy(args) {
+  if (isArray(args)) {
+    return [...args];
+  }
+  const obj = {
+    ...args
+  };
+  return obj;
+}
 
+export function reverseMerge(arg1, arg2) {
+  return [...arg2, ...arg1];
+}
+
+export function filterAttribs(arg) {
+  const { a, b, ...others } = arg;
+  return others;
 }
