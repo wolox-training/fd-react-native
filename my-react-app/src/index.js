21149063dcdda8e2eebb2de96a895de0d03dfe11
Fede <federicodiaz@wolox.com.ar>
2018-10-08 16:51:24 -0300

    now it has the logo and registerServiceWorker as the original but still no response on localhost

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import registerServiceWorker from './registerServiceWorker';
 import './scss/index.scss';
 import Game from './app/components/baseComponents/Game';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
+registerServiceWorker();
