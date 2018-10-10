9a5a1a437599c6d802c14425e4053144f77752a7
Fede <federicodiaz@wolox.com.ar>
2018-10-10 09:58:05 -0300

    adding the reducer

---
+++ my-react-app/src/redux/game/reducer.js
@@ -0,0 +1,42 @@
+import calculateWinner from '../../app/components/baseComponents/Game/utils';
+
+import Actions from './actions';
+
+const initialState = {
+  history: [{ squares: Array(9).fill(null) }],
+  stepNumber: 0,
+  xIsNext: true
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case Actions.handleClick: {
+      const history = state.history.slice(0, state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[action.pos]) {
+        return state;
+      }
+      squares[action.pos] = state.xIsNext ? 'X' : 'O';
+      return {
+        history: history.concat([
+          {
+            squares
+          }
+        ]),
+        stepNumber: history.length,
+        xIsNext: !state.xIsNext
+      };
+    }
+    case Actions.jumpTo: {
+      return {
+        stepNumber: action.stepNumber,
+        xIsNext: state.stepNumber % 2 === 0
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+export default reducer;
