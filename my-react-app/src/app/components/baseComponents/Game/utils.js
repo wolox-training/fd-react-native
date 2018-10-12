b24094c417b80839ca567fb29fc04b3413e17221
Fede <federicodiaz@wolox.com.ar>
2018-10-12 09:31:08 -0300

    spaces on utils.js

--- my-react-app/src/app/components/baseComponents/Game/utils.js
+++ my-react-app/src/app/components/baseComponents/Game/utils.js
@@ -2,8 +2,8 @@ const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8],
 
 function calculateWinner(squares) {
   const winnerLine = lines.find(
-   ([a, b, c]) => squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
- );
- return winnerLine && winnerLine[0];
+    ([a, b, c]) => squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
+  );
+  return winnerLine && winnerLine[0];
 }
 export default calculateWinner;
