b9ddfeb5a28543fa4d091b5fe6f28572318892bc
fededwolox <federico.diaz@wolox.com.ar>
2018-10-24 17:48:54 -0300

    deleting loginformcontainer class

--- my-react-app/src/app/screens/Login/index.js
+++ my-react-app/src/app/screens/Login/index.js
@@ -6,19 +6,9 @@ import actionCreators from '../../../redux/login/actions.js';
 
 import LoginForm from './layout.js';
 
-class LoginFormContainer extends Component {
-  render() {
-    return <LoginForm onSubmit={this.props.login} />;
-  }
-}
-
 const mapDispatchToProps = dispatch => ({
   onSubmit: credentials => dispatch(actionCreators.login(credentials))
 });
-
-LoginFormContainer.propTypes = {
-  login: PropTypes.func.isRequired
-};
 
 export default connect(
   null,
