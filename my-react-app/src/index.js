b093a1f346e8b70e4f19df2c640b3fc809a3cbff
Fede <federicodiaz@wolox.com.ar>
2018-10-24 11:18:36 -0300

    just changing the reactDOM.render to login instead of game

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -5,11 +5,11 @@ import { Provider } from 'react-redux';
 import store from './redux/store.js';
 import './scss/index.scss';
 import registerServiceWorker from './registerServiceWorker';
-import GameContainer from './app/screens/Game/index.js';
+import LoginFormContainer from './app/screens/Login/index.js';
 
 ReactDOM.render(
   <Provider store={store}>
-    <GameContainer />
+    <LoginFormContainer />
   </Provider>,
   document.getElementById('root')
 );
