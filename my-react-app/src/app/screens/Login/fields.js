c711e3be28c8faaaca9e43a5496c932af3173e4c
Fede <federicodiaz@wolox.com.ar>
2018-10-25 10:30:39 -0300

    deleting previous work from react-app-training

--- my-react-app/src/app/screens/Login/fields.js
+++
@@ -1,1 +0,0 @@
-export const FORM_FIELDS = { EMAIL_FIELD: 'email', PASSWORD_FIELD: 'password', FORM_NAME: 'login' };
--- my-react-app/src/services/loginService.js
+++
@@ -1,5 +0,0 @@
-import api from '../config/api.js';
-
-export default {
-  login: credentials => api.post('/login', credentials)
-};
