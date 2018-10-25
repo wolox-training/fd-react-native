29aa6bb885fab293e3b4014a9c1e5be2ab1b363b
Fede <federicodiaz@wolox.com.ar>
2018-10-25 11:52:52 -0300

    changes on title

--- TodoList/app/components/Title.js
+++ TodoList/app/components/Title.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: 'blue',
+    padding: 15,
+  },
+  title: {
+    color: 'white',
+    textAlign: 'center',
+  },
 })
 
 export default class Title extends Component {
 
   render() {
-    return null
+    const {children} = this.props
+
+    return (
+      <View style={styles.header}>
+        <Text style={styles.title}>{children}</Text>
+      </View>
+    )
   }
 }
