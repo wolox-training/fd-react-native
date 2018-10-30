67185ee73d3408676bd94dfee2eeac50971e070b
fededwolox <feded_94@hotmail.com>
2018-10-30 10:01:55 -0300

    components

--- TodoList/src/app/components/Checkbox/styles.js
+++ TodoList/src/app/components/Checkbox/styles.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import colors from '../../../colors';
+import colors from '../../../constants/colors';
 
 const styles = StyleSheet.create({
   box: {
--- TodoList/src/app/components/Footer/styles.js
+++ TodoList/src/app/components/Footer/styles.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import colors from '../../../colors';
+import colors from '../../../constants/colors';
 
 export const styles = StyleSheet.create({
   footer: {
