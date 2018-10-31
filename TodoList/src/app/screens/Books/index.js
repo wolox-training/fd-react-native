39e90d0dc57b24dcd7c298bd6fdb6acf7e26f60d
Fede <federicodiaz@wolox.com.ar>
2018-10-31 13:31:06 -0300

    adding the navigation

---
+++ TodoList/src/app/screens/Books/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class Books extends Component {
+  static navigationOptions = {
+    title: 'Books'
+  };
+
+  render() {
+    return (
+      <View style={{ justifyContent: 'center' }} />
+    );
+  }
+}
+
+export default Books;
--- TodoList/src/index.js
+++ TodoList/src/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
 import store from './redux/store.js';
-import ToDoApp from './app/screens/ToDoList/index'
+import ToDoApp from './app/Routes/RouteNavigation';
 
 class App extends Component {
   render() {
