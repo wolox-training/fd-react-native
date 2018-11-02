6fdd57ff58bc1706a32c01f7ba28e691ea4774cc
Fede <federicodiaz@wolox.com.ar>
2018-11-02 09:34:36 -0300

    few changes on the Navigator

--- TodoList/src/app/components/Navigator/index.js
+++ TodoList/src/app/components/Navigator/index.js
@@ -1,7 +1,8 @@
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 import Books from '../../screens/Books';
 import TodoList from '../../screens/ToDoList';
-import routes from '../../../constants/routes'
+import routes from '../../../constants/routes';
+import colors from '../../../constants/colors'
 
 const TabApp = createBottomTabNavigator({
   [routes.Books]: {
@@ -14,16 +15,18 @@ const TabApp = createBottomTabNavigator({
 },
 {
   tabBarOptions: {
-    activeTintColor: 'red',
-    inactiveTintColor: 'gray'
+    activeTintColor: colors.red,
+    inactiveTintColor: colors.grey
   }
 });
 
-export default StackNavigator = createStackNavigator({
-  ToDoBook: {
+const StackNavigator = createStackNavigator({
+  [routes.ToDoBook]: {
     screen: TabApp,
     navigationOptions: {
       title: 'ToDoBookApp'
     }
   }
 });
+
+export default StackNavigator;
