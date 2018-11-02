3cbfc2b0600128d7ccb888b30109c61f8c5254f9
Fede <federicodiaz@wolox.com.ar>
2018-11-02 09:21:37 -0300

    first advances on the bookList

--- TodoList/.eslintrc.js
+++ TodoList/.eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
     "parser": "babel-eslint",
     "rules": {
   "import/no-extraneous-dependencies": 0,
-  'import/no-extraneous-dependencies': ["error", { devDependencies: true, }],
+  'import/no-extraneous-dependencies': ["error", { "devDependencies": true, }],
   "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  "react/prefer-stateless-function": [0, { "ignorePureComponents": true }],
+  "comma-dangle": ["error", "never"]
 }
 }
--- TodoList/src/app/screens/Books/index.js
+++ TodoList/src/app/screens/Books/index.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, Text } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import Picture from './components/Picture';
+import { styles } from './styles';
 
 class Books extends Component {
   render() {
-    return null
+    const { container } = this.props;
+    return (
+      <View style={styles.container}>
+        <Picture imgUrl={container.image_url} size={40} />
+        <View>
+          <Text>{container.title}</Text>
+          <Text>{container.author}</Text>
+        </View>
+      </View>
+    );
   }
 }
 
-export default Books;
+Books.propTypes = {
+  container: PropTypes.array.isRequired
+};
+
+export default withNavigation(Books);
